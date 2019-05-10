27deba691d1b179b9e1775b0566787d75e437e9f
miyunke <miyunke@iauto.com>
2019-05-10 10:10:05 +0800

    change app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-__author__ = 'Yunke Mi'
-
-...
-async web application
-...
-
-
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -16,13 +9,13 @@ def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
     
-async def init(loop):
+def init(loop):
         app = web.Application(loop=loop)
         app.router.add_route('GET', '/', index)
         srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
         logging.info('server started at http://127.0.0.1:9000...')
         return srv
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(init(loop))
-    loop.run_forever()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
